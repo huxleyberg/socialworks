268f7f842efcc833421290a76e86a7f6e28da02c
chidi <huxleyberg@live.com>
2024-11-16 15:00:44 +0100

    dbconn

--- internal/db/gorm.go
+++ internal/db/gorm.go
@@ -2,20 +2,7 @@ package db
 
 import (
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
-
-// Connect provides functionality for connecting to the database
-func Connect(dialector gorm.Dialector) (*gorm.DB, error) {
-	var db *gorm.DB
-	db, err := gorm.Open(dialector, &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
 
 // Close provides functionality for closing the database connection
 func Close(db *gorm.DB) error {
--- internal/db/providers.go
+++ internal/db/providers.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"log"
+	"time"
 
 	"github.com/pkg/errors"
 	"gorm.io/driver/postgres"
@@ -20,6 +21,17 @@ func ProvidePostgres(postgresDbUrl string) *gorm.DB {
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "failed to connect to postgres"))
 	}
+
+	// Get the underlying sql.DB object to set connection pool configurations
+	sqlDB, err := postgresProviderDB.DB()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "failed to get sql.DB from gorm"))
+	}
+
+	// Set connection pool settings
+	sqlDB.SetMaxOpenConns(30)                  // Maximum number of open connections to the database
+	sqlDB.SetMaxIdleConns(30)                  // Maximum number of idle connections
+	sqlDB.SetConnMaxIdleTime(15 * time.Minute) // Maximum amount of time a connection may be idle
 
 	// Run the migrations
 	if err := Migrate(postgresDbUrl); err != nil {
